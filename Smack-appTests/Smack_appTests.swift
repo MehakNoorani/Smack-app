6250b1b520f7d9c781c017b49b50282ae61dda6d
Mehak Noorani <mehak@miletap.com>
2025-06-25 16:09:01 +0500

    Initial Commit

---
+++ Smack-appTests/Smack_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  Smack_appTests.swift
+//  Smack-appTests
+//
+//  Created by Workwise 1 on 25/06/2025.
+//
+
+import Testing
+@testable import Smack_app
+
+struct Smack_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
